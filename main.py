dc21418977b0a115d82aabaa53627b09c0cf46ad
Asus <louisfuego03@gmail.com>
2022-12-07 22:05:43 +0800

    Ver 2: Tried for more efficiency

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 dataSum = 0.0
 dataAverage = 0.0
 dataLength = 0.0
+
 data = input("Enter a number or press Enter to quit: ")
+
 while data != "": #sets the condition for the loop (while data is not equal to blank)
-    number = float(data)
     dataLength += 1
-    dataSum += number
+    dataSum += float(data)
     dataAverage = dataSum/dataLength
     data = input("Enter a number or just Enter to quit: ")
+
 print("The sum is", dataSum,
       "\nThe average is", dataAverage)
